6bb4b33d9ec5e35e7539e01e538efa8bd5cd6998
Spencer Russell <spencer.f.russell@gmail.com>
2013-12-13 00:17:44 -0500

    comments out the reference code we're using to start PortAudio.jl

--- src/PortAudio.jl
+++ src/PortAudio.jl
@@ -40,61 +40,61 @@ end
 end # module PortAudio
 
 
-type PaStreamCallbackTimeInfo
-    inputBufferAdcTime::PaTime
-    currentTime::PaTime
-    outputBufferDacTime::PaTime
-end
-
-typealias PaStreamCallbackFlags Culong
-
-
-function stream_callback{T}(    input_::Ptr{T}, 
-    output_::Ptr{T}, 
-    frame_count::Culong, 
-    time_info::Ptr{PaStreamCallbackTimeInfo},
-    status_flags::PaStreamCallbackFlags,
-    user_data::Ptr{Void})
-
-
-    println("stfl:$status_flags  \tframe_count:$frame_count")
-
-    ret = 0
-    return convert(Cint,ret)::Cint    #continue stream
-
-end
-
-T=Float32
-stream_callback_c = cfunction(stream_callback,Cint,
-(Ptr{T},Ptr{T},Culong,Ptr{PaStreamCallbackTimeInfo},PaStreamCallbackFlags,Ptr{Void})
-)
-stream_obj = Array(Ptr{PaStream},1)
-
-pa_err = ccall( 
-(:Pa_Initialize,"libportaudio"), 
-PaError,
-(),
-)
-
-println(get_error_text(pa_err))
-
-pa_err = ccall( 
-(:Pa_OpenDefaultStream,"libportaudio"), 
-PaError,
-(Ptr{Ptr{PaStream}},Cint,Cint,PaSampleFormat,Cdouble,Culong,Ptr{Void},Any),
-stream_obj,0,1,0x1,8000.0,4096,stream_callback_c,None
-)
-
-println(get_error_text(pa_err))
-
-function start_stream(stream)
-    pa_err = ccall( 
-    (:Pa_StartStream,"libportaudio"), 
-    PaError,
-    (Ptr{PaStream},),
-    stream
-    )    
-    println(get_error_text(pa_err))
-end
-
-end #module
+#type PaStreamCallbackTimeInfo
+#    inputBufferAdcTime::PaTime
+#    currentTime::PaTime
+#    outputBufferDacTime::PaTime
+#end
+#
+#typealias PaStreamCallbackFlags Culong
+#
+#
+#function stream_callback{T}(    input_::Ptr{T}, 
+#    output_::Ptr{T}, 
+#    frame_count::Culong, 
+#    time_info::Ptr{PaStreamCallbackTimeInfo},
+#    status_flags::PaStreamCallbackFlags,
+#    user_data::Ptr{Void})
+#
+#
+#    println("stfl:$status_flags  \tframe_count:$frame_count")
+#
+#    ret = 0
+#    return convert(Cint,ret)::Cint    #continue stream
+#
+#end
+#
+#T=Float32
+#stream_callback_c = cfunction(stream_callback,Cint,
+#(Ptr{T},Ptr{T},Culong,Ptr{PaStreamCallbackTimeInfo},PaStreamCallbackFlags,Ptr{Void})
+#)
+#stream_obj = Array(Ptr{PaStream},1)
+#
+#pa_err = ccall( 
+#(:Pa_Initialize,"libportaudio"), 
+#PaError,
+#(),
+#)
+#
+#println(get_error_text(pa_err))
+#
+#pa_err = ccall( 
+#(:Pa_OpenDefaultStream,"libportaudio"), 
+#PaError,
+#(Ptr{Ptr{PaStream}},Cint,Cint,PaSampleFormat,Cdouble,Culong,Ptr{Void},Any),
+#stream_obj,0,1,0x1,8000.0,4096,stream_callback_c,None
+#)
+#
+#println(get_error_text(pa_err))
+#
+#function start_stream(stream)
+#    pa_err = ccall( 
+#    (:Pa_StartStream,"libportaudio"), 
+#    PaError,
+#    (Ptr{PaStream},),
+#    stream
+#    )    
+#    println(get_error_text(pa_err))
+#end
+#
+#end #module
