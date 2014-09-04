4ebd7b946d553c8e7ff53026cce978f64546b633
Spencer Russell <spencer.f.russell@gmail.com>
2014-09-04 09:18:03 -0400

    adds WinRPM to REQUIRE and build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -8,16 +8,22 @@ libportaudio = library_dependency("libportaudio")
 libsndfile = library_dependency("libsndfile")
 
 # TODO: add other providers with correct names
-provides(AptGet, {"portaudio19-dev" => libportaudio})
-provides(AptGet, {"libsndfile1-dev" => libsndfile})
-provides(Pacman, {"portaudio" => libportaudio})
-provides(Pacman, {"libsndfile" => libsndfile})
+provides(AptGet, "portaudio19-dev", libportaudio)
+provides(AptGet, "libsndfile1-dev", libsndfile)
+provides(Pacman, "portaudio", libportaudio)
+provides(Pacman, "libsndfile", libsndfile)
 
 
 @osx_only begin
     using Homebrew
-    provides(Homebrew.HB, {"portaudio" => libportaudio})
-    provides(Homebrew.HB, {"libsndfile" => libsndfile})
+    provides(Homebrew.HB, "portaudio", libportaudio)
+    provides(Homebrew.HB, "libsndfile", libsndfile)
+end
+
+@windows_only begin
+    using WinRPM
+    provides(WinRPM.RPM, "portaudio", libportaudio, os = :Windows)
+    provides(WinRPM.RPM, "libsndfile", libsndfile, os = :Windows)
 end
 
 @BinDeps.install [:libportaudio => :libportaudio,
