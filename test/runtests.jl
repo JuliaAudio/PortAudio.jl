3fbd31e18af90e3492c4fbb27f1f1f679da9b850
Brandon Taylor <brandon.taylor221@gmail.com>
2021-05-25 11:29:33 -0400

    more coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,22 @@
 
 using Logging: Debug
 using PortAudio
-using PortAudio: Pa_GetDefaultInputDevice, Pa_GetDefaultOutputDevice, Pa_GetDeviceInfo, Pa_GetHostApiInfo, PortAudioDevice, @stderr_as_debug
-using Test
+using PortAudio: 
+    combine_default_sample_rates,
+    handle_status,
+    Pa_GetDefaultInputDevice, 
+    Pa_GetDefaultOutputDevice, 
+    Pa_GetDeviceInfo, 
+    Pa_GetHostApiInfo, 
+    Pa_Initialize,
+    PA_OUTPUT_UNDERFLOWED,
+    Pa_Terminate,
+    PortAudioDevice,
+    recover_xrun,
+    seek_alsa_conf,
+    @stderr_as_debug
 using SampledSignals
+using Test
 
 @testset "Debug messages" begin
     @test_logs (:debug, "hi") min_level = Debug @test_nowarn @stderr_as_debug begin
@@ -33,6 +46,10 @@ end
 end
 
 if !isempty(PortAudio.devices())
+    # make sure we can terminate, then reinitialize
+    Pa_Terminate()
+    @stderr_as_debug Pa_Initialize()
+
     # these default values are specific to my machines
     inidx = Pa_GetDefaultInputDevice()
     default_indev = PortAudioDevice(Pa_GetDeviceInfo(inidx), inidx).name
@@ -47,13 +64,32 @@ if !isempty(PortAudio.devices())
             close(stream)
             @test size(buf) == (round(Int, 5 * samplerate(stream)), nchannels(stream.source))
             println("Playing back recording...")
-            stream = PortAudioStream(0, 2)
-            write(stream, buf)
-            close(stream)
+            PortAudioStream(0, 2) do stream
+                write(stream, buf)
+            end
             println("Testing pass-through")
             stream = PortAudioStream(2, 2)
+            sink = stream.sink
+            source = stream.source
+            @test sprint(show, typeof(sink)) == "PortAudioSink{Float32}"
+            @test sprint(show, typeof(source)) == "PortAudioSource{Float32}"
+            @test sprint(show, sink) == "2-channel PortAudioSink{Float32}($(repr(default_indev)))"
+            @test sprint(show, source) == "2-channel PortAudioSource{Float32}($(repr(default_outdev)))"
             write(stream, stream, 5s)
+            @test_throws ErrorException("""
+                Attempted to close PortAudioSink or PortAudioSource.
+                Close the containing PortAudioStream instead
+                """
+            ) close(sink)
+            @test_throws ErrorException("""
+                Attempted to close PortAudioSink or PortAudioSource.
+                Close the containing PortAudioStream instead
+                """
+            ) close(source)
             close(stream)
+            @test !isopen(stream)
+            @test !isopen(sink)
+            @test !isopen(source)
             println("done")
         end
         @testset "Samplerate-converting writing" begin
@@ -77,8 +113,30 @@ if !isempty(PortAudio.devices())
               2 channel source: "$default_indev\"""", String(take!(io)))
             close(stream)
         end
-        @testset "Error on wrong name" begin
+        @testset "Error handling" begin
             @test_throws ErrorException PortAudioStream("foobarbaz")
+            @test_throws ErrorException PortAudioStream(default_indev, "foobarbaz")
+            @test_logs (:warn, "libportaudio: Output underflowed") handle_status(PA_OUTPUT_UNDERFLOWED)
+            @test_throws ErrorException("libportaudio: PortAudio not initialized") handle_status(-10000)
+            @test_throws ErrorException("""
+                Could not find ALSA config directory. Searched:
+                
+
+                If ALSA is installed, set the "ALSA_CONFIG_DIR" environment
+                variable. The given directory should have a file "alsa.conf".
+                
+                If it would be useful to others, please file an issue at
+                https://github.com/JuliaAudio/PortAudio.jl/issues
+                with your alsa config directory so we can add it to the search
+                paths.
+                """
+            ) seek_alsa_conf([])
+            @test_throws ErrorException("""
+                Can't open duplex stream with mismatched samplerates (in: 0, out: 1).
+                Try changing your sample rate in your driver settings or open separate input and output
+                streams.
+                """
+            ) combine_default_sample_rates(1, 0, 1, 1)
         end
         # no way to check that the right data is actually getting read or written here,
         # but at least it's not crashing.
