5798a9a4d47baefa22f06192003f3570aa173f3f
Albert Gnadt <20693045+gnadt@users.noreply.github.com>
2024-04-17 14:28:15 -0400

    Revert "Make ALSA_PLUGIN_DIR env variable available during precompilation"
    This reverts commit f7c24b655f7975b8dfc00444f3213f0bb0ac8ffa.

--- src/PortAudio.jl
+++ src/PortAudio.jl
@@ -1,14 +1,6 @@
 module PortAudio
 
-if Sys.islinux()
-    using alsa_plugins_jll
-    # the plugin folder will contain plugins for, critically, PulseAudio
-    plugin_folder = "ALSA_PLUGIN_DIR"
-    if plugin_folder ∉ keys(ENV) && alsa_plugins_jll.is_available()
-        ENV[plugin_folder] = joinpath(alsa_plugins_jll.artifact_dir, "lib", "alsa-lib")
-    end
-end
-
+using alsa_plugins_jll: alsa_plugins_jll
 import Base:
     close,
     eltype,
@@ -144,6 +136,11 @@ function __init__()
             ENV[config_folder] =
                 seek_alsa_conf(("/usr/share/alsa", "/usr/local/share/alsa", "/etc/alsa"))
         end
+        # the plugin folder will contain plugins for, critically, PulseAudio
+        plugin_folder = "ALSA_PLUGIN_DIR"
+        if plugin_folder ∉ keys(ENV) && alsa_plugins_jll.is_available()
+            ENV[plugin_folder] = joinpath(alsa_plugins_jll.artifact_dir, "lib", "alsa-lib")
+        end
     end
     initialize()
     atexit(() -> terminate())
@@ -248,7 +245,7 @@ function read_or_write(a_function, buffer, use_frames = buffer.frames_per_buffer
     pointer_to = buffer.pointer_to
     data = buffer.data
     handle_status(
-        if acquire_lock
+        if acquire_lock 
             # because we're calling Pa_ReadStream and Pa_WriteStream from separate threads,
             # we put a lock around these calls
             lock(
@@ -739,7 +736,7 @@ function PortAudioStream(
     stream_lock = ReentrantLock(),
     user_data = C_NULL,
     warn_xruns = true,
-    writer = SampledSignalsWriter(),
+    writer = SampledSignalsWriter(),   
 )
     input_channels_filled = fill_max_channels(
         "input",
